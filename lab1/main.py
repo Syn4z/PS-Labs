4cf566923b0ca25ccf66a8899de5d32dcdf0a7d3
syn4z <iatcosorin797@gmail.com>
2024-03-09 15:52:44 +0200

    feat: add lab1 task1

---
+++ lab1/main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from step1 import randomNoise, digitalFilter
+
+
+Ts1 = 0.01
+Ts2 = 0.001
+
+# Plotting the random noise 1.1-1.4
+plt.figure(figsize=(10, 8))
+plt.subplot(2, 2, 1)
+randomNoise(Ts1, True)
+plt.subplot(2, 2, 2)
+randomNoise(Ts1, False)
+plt.subplot(2, 2, 3)
+randomNoise(Ts2, True)
+plt.subplot(2, 2, 4)
+randomNoise(Ts2, False)
+plt.tight_layout()
+plt.show()
+
+# Plotting the digital filter 1.5-1.6
+plt.figure(figsize=(10, 8))
+plt.subplot(1, 2, 1)
+digitalFilter(Ts1)
+plt.subplot(1, 2, 2)
+digitalFilter(Ts2)
+plt.tight_layout()
+plt.show()
---
+++ lab1/step1.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def randomNoise(Ts=0.01, plot=True):
+    t = np.arange(0, 5, Ts)
+    x = np.random.rand(len(t))
+
+    if Ts == 0.01:
+        xLabel = 'x1'
+        color = 'r'
+    elif Ts == 0.001:
+        xLabel = 'x2'
+        color = 'g'  
+
+    if plot:
+        plt.plot(t, x, label=xLabel, color=color)
+        plt.title(f'Example of random noise using plot, Ts={Ts}s')
+    else:
+        plt.hist(x, bins=20, label=xLabel, color=color)    
+        plt.title(f'Example of random noise using hist, Ts={Ts}s')
+    plt.grid(True)
+    plt.xlabel('time(s)')
+    plt.ylabel('function y(t)')
+    plt.legend()
+
+def digitalFilter(Ts=0.01):
+    om0 = 2 * np.pi
+    dz = 0.005
+    A = 1
+    oms = om0 * Ts
+
+    a = np.zeros(3)
+    a[0] = 1 + 2 * dz * oms + oms ** 2
+    a[1] = -2 * (1 + dz * oms)
+    a[2] = 1
+
+    b = np.zeros(1)
+    b[0] = A * 2 * oms ** 2
+
+    t = np.arange(0, 50, Ts)
+    x = np.random.rand(len(t))
+    y1 = np.convolve(x, b, mode='same') / a[0] - np.convolve(x, a[1:], mode='same') / a[0]
+
+    if Ts == 0.01:
+        xLabel = 'x1'
+        color = 'r'
+    elif Ts == 0.001:
+        xLabel = 'x2'
+        color = 'g'
+
+    plt.plot(t, y1, label=xLabel, color=color)
+    plt.grid(True)
+    plt.xlabel('time(s)')
+    plt.ylabel('function y(t)')
+    plt.title(f'Filtering noise with a second-order filter Ts={Ts}s')
+    plt.legend()
